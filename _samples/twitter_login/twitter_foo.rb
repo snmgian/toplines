f84cbb4cc2a11922ab02b816d9ba02d5b4baddc3
gian <snmgian@gmail.com>
2012-06-13 20:18:49 -0300

    removing comments

--- _samples/twitter_login/twitter_foo.rb
+++ _samples/twitter_login/twitter_foo.rb
@@ -1,10 +1,5 @@
 require 'sinatra/base'
 
-#Twitter::Login.new(
-  #:consumer_key => 'AtE0BY2z371vYIWxQ4Xc8w', 
-  #:secret => '6SBAdsPW33bsIuFnImXqCNtjEQO18osTKPF1EOnIyU'
-#)
-
 class TwitterFoo < Sinatra::Base
   get '/' do
     'Twitter FOO'
@@ -14,5 +9,3 @@ class TwitterFoo < Sinatra::Base
     'You are authorized, welcome!'
   end
 end
-
-# ruby sinatra_foo.rb
